d63075c5f63543a1c5f59ff55df68d532c9e032d
Jean Gabriel Silva Lima <jeangabrielslima@gmail.com>
2025-03-18 14:57:14 -0300

    Adicionando estrutura de autenticacao JWT

---
+++ src/backend/routes/authRoutes.js
@@ -0,0 +1,12 @@
+// routes/authRoutes.js
+import express from 'express';
+import { register, login, verifyToken } from '../controllers/authController.js';
+
+const router = express.Router();
+
+// Rotas de autenticação
+router.post('/register', register);
+router.post('/login', login);
+router.get('/verify', verifyToken);
+
+export default router;
