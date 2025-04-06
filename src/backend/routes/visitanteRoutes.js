dbad8e0bcae5000231dd23b82ab8436422493636
Gabriel Alves Developer <110769950+gabrielteg@users.noreply.github.com>
2025-04-06 08:08:40 -0300

    Update visitanteRoutes.js

--- src/backend/routes/visitanteRoutes.js
+++ src/backend/routes/visitanteRoutes.js
@@ -7,12 +7,15 @@ import {
   deletarVisitante,
 } from "../controllers/visitanteController.js";
 
+import { verifyToken } from "../controllers/authController.js";  
+import { isAdmin } from "../middleware/authMiddleware.js"; 
+
 const router = express.Router();
 
-router.post("/visitantes", criarVisitante);
-router.get("/visitantes", listarVisitantes);
-router.get("/visitantes/:id", buscarVisitantePorId);
-router.put("/visitantes/:id", atualizarVisitante);
-router.delete("/visitantes/:id", deletarVisitante);
+router.post("/visitantes", verifyToken, criarVisitante); 
+router.get("/visitantes", verifyToken, isAdmin, listarVisitantes); 
+router.get("/visitantes/:id", verifyToken, buscarVisitantePorId); 
+router.put("/visitantes/:id", verifyToken, atualizarVisitante); 
+router.delete("/visitantes/:id", verifyToken, isAdmin, deletarVisitante); 
 
 export default router;
