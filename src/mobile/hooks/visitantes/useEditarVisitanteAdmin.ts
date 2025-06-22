31d95ad818f3d5601e81f91cd1a2cec766f8aa3b
guilhermefialho <92562186+guilhermefialho@users.noreply.github.com>
2025-06-22 10:36:04 -0300

    correção versão react-query

--- src/mobile/hooks/visitantes/useEditarVisitanteAdmin.ts
+++ src/mobile/hooks/visitantes/useEditarVisitanteAdmin.ts
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { useMutation } from '@tanstack/react-query';
 import Constants from 'expo-constants';
 import { obterToken } from '@/utils/auth';
 
